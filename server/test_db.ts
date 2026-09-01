a725e9fb4c863089ab45d6d02707e8ad5549b662
Phatchara <Phatcharakrub@gmail.com>
2026-09-01 11:26:06 +0700

    chore: remove temporary test_db.ts script

--- server/test_db.ts
+++
@@ -1,8 +0,0 @@
-import { getPrisma } from "./src/prisma.js";
-
-async function test() {
-  const prisma = getPrisma();
-  console.log("Categories:", await prisma.category.count());
-  console.log("Systems:", await prisma.relatedSystem.count());
-}
-test().catch(console.error).finally(() => process.exit(0));
